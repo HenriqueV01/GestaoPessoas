1d9d069c9b366eb7894b28f637d3bdcb782a7048
HenriqueV01 <henrique.unibratec01@gmail.com>
2024-05-16 11:39:57 -0300

    Primeiro commit

---
+++ src/test/java/br/com/gestaopessoas/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gestaopessoas.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
